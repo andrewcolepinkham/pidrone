691628c2c28fbedd2fea21abd72f09af3eb32888
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2021-02-02 22:13:00 -0700

    test

---
+++ bmp180_tests/test_bmp.py
@@ -0,0 +1,32 @@
+import bmpsensor
+import time
+from datetime import datetime
+import csv
+rows = []
+count = 0
+while count < 1000:
+    temp, pressure, altitude = bmpsensor.readBmp180()
+    tm = datetime.now().time() # time object
+    # print("Temperature is ", temp)  # degC
+    # print("Pressure is ", pressure)  # Pressure in Pa
+    # print("Altitude is ", altitude)  # Altitude in meters
+    # print("\n")
+    # print(tm)
+    rows.append([temp, pressure, altitude, tm, count])
+    count+=1
+    time.sleep(.01)
+
+filename = "bmp180_readings.csv"
+fields = ['Tempurature (degC)', 'Pressure (Pa)', 'Altitude (m)', 'Time']
+# writing to csv file
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
+
+    # writing the fields
+    csvwriter.writerow(fields)
+
+    # writing the data rows
+    csvwriter.writerows(rows)
+    
+    print("done")
